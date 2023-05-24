62aeb89f80ea42919d7cf0cc9ce9506f8a64ddf0
suchjose84 <suchjose@live.com>
2023-05-25 00:04:38 +0800

    connect to react

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 app
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(cors())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  })
   .use(express.json())
   .use(express.urlencoded({
     extended: true
@@ -15,6 +21,7 @@ app
   .use('/', require('./routes'));
 
 const db = require('./models');
+const bodyParser = require('body-parser');
 
 db.mongoose
   .connect(db.url, {

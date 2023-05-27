4e67f25b08d66c0890e4498f87e68d3527eb871c
suchjose84 <suchjose@live.com>
2023-05-28 02:04:31 +0800

    polish more

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-router.use('/api', swaggerUi.serve);
-router.get('/api', swaggerUi.setup(swaggerDocument));
+router.use('/docs-api', swaggerUi.serve);
+router.get('/docs-api', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;

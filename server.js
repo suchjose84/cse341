34ac1017d8587897cc485395d038fb1a67409a90
suchjose84 <suchjose@live.com>
2023-05-27 20:51:05 +0800

    polish

--- server.js
+++ server.js
@@ -8,13 +8,6 @@ const app = express();
 
 app
   .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-  })
   .use(cors())
   .use('/', require('./routes'));
 

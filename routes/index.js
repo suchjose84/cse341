564b23ab34c001e5c3b1813337ec857a457520f0
suchjose84 <suchjose@live.com>
2023-05-27 12:44:39 +0800

    finish up

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ routes.use('/', require('./swagger'));
 routes.use('/', contact);
 routes.use('/', (docData = (req, res) => {
     let docData = {
-      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+      documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
     };
     res.send(docData);
   })

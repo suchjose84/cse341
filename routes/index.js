a384b7743235a2b975a41b77d471a7b828a111bb
suchjose84 <suchjose@live.com>
2023-05-28 02:09:29 +0800

    polish some more

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ routes.use('/', require('./swagger'));
 routes.use('/', contact);
 routes.use('/', (docData = (req, res) => {
     let docData = {
-      documentationURL: 'https://cse341-mw5a.onrender.com/api',
+      documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
     };
     res.send(docData);
   })
